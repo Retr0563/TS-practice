d06508e428348397a5b261373c6a49ec880205d3
Retr0 <nanayawbee2014@gmail.com>
2025-10-17 12:35:42 +0000

    counter App

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,9 @@
-import AdminInfo from "./components/AdminInfo";
-import UserInfo from "./components/UserInfo";
+import Counter from "./Components/Counter";
 
 const App = () => {
   return (
     <div>
-      <h1>User Info</h1>
-      <UserInfo
-        name="John"
-        email="j23@gamil.com"
-        age={24}
-        location={["Africa", "USA"]}
-      />
-      <h1>Admin Info</h1>
-      <AdminInfo
-        name="Michael"
-        email="retr0@gmail.com "
-        location={["Earth", " Ghana"]}
-        admin="yes"
-      />
+      <Counter />
     </div>
   );
 };
---
+++ src/Components/Counter.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+const Counter = () => {
+  const [count, setCount] = useState<number>(0);
+
+  return (
+    <div>
+      <h1>Counter App</h1>
+      <p>COunt : {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+    </div>
+  );
+};
+export default Counter;
